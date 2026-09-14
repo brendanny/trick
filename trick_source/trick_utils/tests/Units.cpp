a80aee2ae5f484e468474d14af96d068fcab8ad4
brendan fattig <brendan@fattig.net>
2026-09-14 18:58:14 -0500

    build(cmake): add native utility archives and focused tests

---
+++ trick_source/trick_utils/tests/Units.cpp
@@ -0,0 +1,17 @@
+#include "trick/Unit.hh"
+
+#include <gtest/gtest.h>
+
+TEST(UnitsConsumer, ConvertsLengthAndTemperature)
+{
+    Unit metres("m");
+    EXPECT_DOUBLE_EQ(metres.Convert_to(2.0, "cm"), 200.0);
+    Unit celsius("degC");
+    EXPECT_NEAR(celsius.Convert_to(100.0, "degF"), 212.0, 1e-10);
+}
+
+TEST(UnitsConsumer, RejectsIncompatibleDimensions)
+{
+    Unit metres("m");
+    EXPECT_THROW(metres.Convert_to(1.0, "s"), Unit::CONVERSION_ERROR);
+}
